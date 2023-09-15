c000667a0852c6a19770ed010fbbdd016bf2a538
willydekeyser <ma074172@skynet.be>
2023-09-15 10:00:29 +0200

    Setup Spring Security - Cloud Gateway - Angular Tutorial

---
+++ SpringResourceServer01/src/test/java/willydekeyser/SpringResourceServer01ApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResourceServer01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SpringResourceServer02/src/main/java/willydekeyser/SpringResourceServer02Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceServer02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResourceServer02Application.class, args);
+	}
+
+}
