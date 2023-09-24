aaafb40ad3ea4f84f514213d7261dcd76b06e94c
willydekeyser <ma074172@skynet.be>
2023-09-24 18:09:25 +0200

    Update Spring Resource Server03

--- SpringResourceServer03/src/main/java/willydekeyser/config/SecurityConfig.java
+++ SpringResourceServer03/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -3,23 +3,42 @@ package willydekeyser.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
 	String issuerUri;
 	
+	private static final String JWT_ROLE_NAME = "authorities";
+    private static final String ROLE_PREFIX = "";
+    
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
 				.authorizeHttpRequests(auth -> auth
 						.anyRequest().authenticated())
 				.oauth2ResourceServer(oauth2 -> oauth2
-						.jwt(jwt -> jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))))
+						.jwt(jwt -> jwt
+								.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
+								.jwtAuthenticationConverter(jwtAuthenticationConverter())))
 				.build();
 	}
+	
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(JWT_ROLE_NAME);
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(ROLE_PREFIX );
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
 }
--- SpringResourceServer03/src/main/java/willydekeyser/controller/UsersController.java
+++ SpringResourceServer03/src/main/java/willydekeyser/controller/UsersController.java
@@ -2,6 +2,7 @@ package willydekeyser.controller;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,9 @@ public class UsersController {
 	}
 	
 	@GetMapping("/users")
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 	public List<Users> findAllUsers() {
 		return usersService.findAllUsers();
 	}
 	
-	@GetMapping("/test")
-	public List<Users> findAllTest() {
-		return usersService.findAllUsers();
-	}
 }
