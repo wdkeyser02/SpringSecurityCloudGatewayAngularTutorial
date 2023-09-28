6f88075b899c66e6f78b8eca1bd8ef4fbfaf0061
willydekeyser <ma074172@skynet.be>
2023-09-28 10:29:12 +0200

    Update Users to record

--- SpringResourceServer03/src/main/java/willydekeyser/entity/Test.java
+++ SpringResourceServer03/src/main/java/willydekeyser/entity/Test.java
@@ -2,8 +2,8 @@ package willydekeyser.entity;
 
 import java.util.List;
 
-public record Users(String username, String password, Boolean enabled, List<String> authorities) {
-	
+public record Test(String username, String password, Boolean enabled, List<String> authorities) {
+
 	public void addAuthority(String authority) {
 		this.authorities.add(authority);
 	}	
