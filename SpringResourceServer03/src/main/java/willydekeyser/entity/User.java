d275cd05a457009cf380e87ead12118ccad39d49
willydekeyser <ma074172@skynet.be>
2023-09-29 12:36:17 +0200

    Update User

---
+++ SpringResourceServer03/src/main/java/willydekeyser/entity/User.java
@@ -0,0 +1,10 @@
+package willydekeyser.entity;
+
+import java.util.List;
+
+public record User(String username, String password, Boolean enabled, List<String> authorities) {
+	
+	public void addAuthority(String authority) {
+		this.authorities.add(authority);
+	}	
+}
