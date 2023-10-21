ba5f40eb5dfeac3807377aa79a7e117116148d07
willydekeyser <ma074172@skynet.be>
2023-10-21 14:27:28 +0200

    Update schema.sql

--- SpringAuthorizationServer/src/main/resources/schema.sql
+++ SpringAuthorizationServer/src/main/resources/schema.sql
@@ -6,6 +6,22 @@ create table if not exists users
 	enabled boolean not null
 );
 
+create table if not exists usersinfo
+(
+	username varchar(200) not null primary key,
+	isAccountNonExpired boolean not null,
+	isAccountNonLocked boolean not null,
+	isCredentialsNonExpired boolean not null,
+	securityQuestion varchar(200) not null,
+	securityAnswer varchar(200) not null,
+	mfaSecret varchar(200) not null,
+	mfaKeyId varchar(200) not null,
+	mfaEnabled boolean not null,
+	mfaRegistered boolean not null,
+	securityQuestionEnabled boolean not null,
+	constraint fk_usersinfo_users foreign key (username) references users (username)
+);
+
 create table if not exists authorities
 (
 	username varchar(200) not null,
