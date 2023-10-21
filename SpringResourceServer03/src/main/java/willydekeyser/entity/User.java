d437eeb21e0cc45b96c838f2417b6f6a2d51e894
willydekeyser <ma074172@skynet.be>
2023-10-21 10:37:12 +0200

    Update SpringResourceServer03

--- SpringResourceServer03/src/main/java/willydekeyser/entity/User.java
+++ SpringResourceServer03/src/main/java/willydekeyser/entity/User.java
@@ -2,7 +2,22 @@ package willydekeyser.entity;
 
 import java.util.List;
 
-public record User(String username, String password, Boolean enabled, List<String> authorities) {
+public record User(
+		String username, 
+		String password, 
+		boolean enabled,
+		boolean isAccountNonExpired,
+		boolean isAccountNonLocked,
+		boolean isCredentialsNonExpired,
+		List<String> authorities,
+		String securityQuestion,
+		String answer,
+		String mfaSecret,
+		String mfaKeyId,
+		boolean mfaEnabled,
+		boolean mfaRegistered,
+		boolean securityQuestionEnabled
+		) {
 	
 	public void addAuthority(String authority) {
 		this.authorities.add(authority);
--- SpringResourceServer03/src/main/java/willydekeyser/service/UsersService.java
+++ SpringResourceServer03/src/main/java/willydekeyser/service/UsersService.java
@@ -20,18 +20,37 @@ public class UsersService {
 	}
 
 	public List<User> findAllUsers() {
-		var sql = "SELECT user.username, user.password, user.enabled, authorities.authority FROM users user LEFT JOIN authorities on user.username = authorities.username";
+		var sql = """
+				SELECT user.username, user.password, user.enabled, authorities.authority, userinfo.isAccountNonExpired, 
+				userinfo.isAccountNonLocked, userinfo.isCredentialsNonExpired, userinfo.securityQuestion, 
+				userinfo.securityAnswer, userinfo.mfaSecret, userinfo.mfaKeyId, userinfo.mfaEnabled, 
+				userinfo.mfaRegistered, userinfo.securityQuestionEnabled  
+				FROM usersinfo userinfo, users user 
+				LEFT JOIN authorities on user.username = authorities.username 
+				WHERE user.username = userinfo.username;
+				""";
 		List<User> usersLijst = new ArrayList<>();
 		return jdbcTemplate.query(sql, rs -> {
 			Map<String, User> usersByUsername = new HashMap<>();
 			while (rs.next()) {
 				String username = rs.getString("username");
 				String password = rs.getString("password");
-				Boolean enabled = rs.getBoolean("enabled");
+				boolean enabled = rs.getBoolean("enabled");
+				boolean isAccountNonExpired = rs.getBoolean("isAccountNonExpired");
+				boolean isAccountNonLocked = rs.getBoolean("isAccountNonLocked");
+				boolean isCredentialsNonExpired = rs.getBoolean("isCredentialsNonExpired");
+				String securityQuestion = rs.getString("securityQuestion");
+				String securityAnswer = rs.getString("securityAnswer");
+				String mfaSecret = rs.getString("mfaSecret");
+				String mfaKeyId = rs.getString("mfaKeyId");
+				boolean mfaEnabled = rs.getBoolean("mfaEnabled");
+				boolean mfaRegistered = rs.getBoolean("mfaRegistered");
+				boolean securityQuestionEnabled = rs.getBoolean("securityQuestionEnabled");
 				String authority = rs.getString("authority");
 				User user = usersByUsername.get(username);
 				if(user == null) {
-					user = new User(username, password, enabled, new ArrayList<>());
+					user = new User(username, password, enabled, isAccountNonExpired, isAccountNonLocked, isCredentialsNonExpired, new ArrayList<>(),
+							securityQuestion, securityAnswer, mfaSecret, mfaKeyId, mfaEnabled, mfaRegistered, securityQuestionEnabled);
 					usersByUsername.put(username, user);
 					usersLijst.add(user);					
 				}
