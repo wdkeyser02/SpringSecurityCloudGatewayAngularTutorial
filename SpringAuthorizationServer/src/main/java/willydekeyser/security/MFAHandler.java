3062c44305ff1f38eed7ed16a5ce20858164fb5b
willydekeyser <ma074172@skynet.be>
2023-10-21 12:45:09 +0200

    Update MFAHandler

--- SpringAuthorizationServer/src/main/java/willydekeyser/security/MFAHandler.java
+++ SpringAuthorizationServer/src/main/java/willydekeyser/security/MFAHandler.java
@@ -2,10 +2,12 @@ package willydekeyser.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
@@ -13,11 +15,13 @@ import org.springframework.security.web.context.SecurityContextRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import willydekeyser.user.User;
 
 
 public class MFAHandler implements AuthenticationSuccessHandler {
 
 	private final SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+	private final AuthenticationSuccessHandler mfaNotEnabled = new SavedRequestAwareAuthenticationSuccessHandler();
 
 	private final AuthenticationSuccessHandler authenticationSuccessHandler;
 	private final String authority;
@@ -37,6 +41,13 @@ public class MFAHandler implements AuthenticationSuccessHandler {
 			HttpServletRequest request, 
 			HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
+		if (authentication instanceof UsernamePasswordAuthenticationToken) {
+			User user = (User) authentication.getPrincipal();
+			if (!user.mfaEnabled()) {
+				mfaNotEnabled.onAuthenticationSuccess(request, response, authentication);
+				return;
+			}
+		}
 		saveAuthentication(request, response, new MFAAuthentication(authentication, authority));
 		this.authenticationSuccessHandler.onAuthenticationSuccess(request, response, authentication);
 
